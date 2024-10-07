cde4790599926f21b1dad2c0444203d8f5f77c95
Amir Kangarloo <amirkangarloo1051@gmail.com>
2024-10-07 11:22:21 +0330

    add error

--- src/domain/product/product.controller.ts
+++ src/domain/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Headers } from '@nestjs/common';
+import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DbService } from 'src/db/db.service';
 import { ProductService } from 'src/domain/product/product.service';
@@ -6,6 +6,7 @@ import { ProductService } from 'src/domain/product/product.service';
 @Controller({ path: 'product' })
 @ApiTags('Product')
 export class ProductController {
+  private counter = 0;
   constructor(
     private readonly db: DbService,
     private readonly productService: ProductService,
@@ -13,8 +14,11 @@ export class ProductController {
 
   @Get()
   @ApiOperation({ summary: 'دریافت لیست محصولات' })
-  getProducts(@Headers('token') token: string) {
-    this.db.validateUserToken(token);
+  getProducts() {
+    this.counter++;
+    if (this.counter % 5 === 0) {
+      throw new InternalServerErrorException('you found new error :)');
+    }
     return this.productService.getProducts();
   }
 }
