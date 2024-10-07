314e8d5e4e9ca0438189170dcd46dab12f97c141
Amir Kangarloo <amirkangarloo1051@gmail.com>
2024-10-07 10:10:22 +0330

    change doc root

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('v-1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/doc', app, document);
+  SwaggerModule.setup('/', app, document);
   await app.listen(3000);
 }
 bootstrap();
